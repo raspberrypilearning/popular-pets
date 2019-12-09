8273ec888f1c9777a5a2b9964ac1a6dff4a284c3
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2019-12-09 13:19:17 +0000

    New translations popular-pets-finished-popular-pets.py (French)

--- fr-FR/resources/popular-pets-finished-popular-pets.py
+++ fr-FR/resources/popular-pets-finished-popular-pets.py
@@ -1,17 +1,17 @@
 #!/bin/python3
 
-pygal d'importation
+import pygal
 
-piece = pygal.Pie ()
-piechart.title = 'Favorite Pet'
-piechart.add ( 'Dog', 6)
-piece.add ('Cat', 4)
-piece.add ('Hamster', 3)
-piece.add ('Poisson', 2)
-piece.add ('Serpent', 1)
-piece.render ()
+camembert = pygal.Pie()
+camembert.title = 'Animaux de compagnie préférés'
+camembert.add('Chien', 6)
+camembert.add('Chat', 4)
+camembert.add('Hamster', 3)
+camembert.add('Poisson', 2)
+camembert.add('Serpent', 1)
+camembert.render()
 
-barchart = pygal.Bar ()
+histogramme = pygal.Bar()
 barchart.title = 'Animal favori'
 barchart.add ('Chien', 6)
 barchart.add ('Cat', 4)

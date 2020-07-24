412ea30d353afcb833dc12de90289bc2b32fb814
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-07-24 17:39:57 +0100

    New translations popular-pets-finished-popular-pets.py (Hungarian)

--- hu-HU/resources/popular-pets-finished-popular-pets.py
+++ hu-HU/resources/popular-pets-finished-popular-pets.py
@@ -2,62 +2,62 @@
 
 import pygal
 
-piechart = pygal.Pie()
-piechart.title = 'Favourite Pet'
-piechart.add('Dog', 6)
-piechart.add('Cat', 4)
-piechart.add('Hamster', 3)
-piechart.add('Fish', 2)
-piechart.add('Snake', 1)
-piechart.render()
+kordiagram = pygal.Pie()
+kordiagram.title = 'Kedvenc kisállat'
+kordiagram.add('Kutya', 6)
+kordiagram.add('Macska', 4)
+kordiagram.add('Hörcsög', 3)
+kordiagram.add('Hal', 2)
+kordiagram.add('Kígyó', 1)
+kordiagram.render()
 
-barchart = pygal.Bar()
-barchart.title = 'Favourite Pet'
-barchart.add('Dog', 6)
-barchart.add('Cat', 4)
-barchart.add('Hamster', 3)
-barchart.add('Fish', 2)
-barchart.add('Snake', 1)
-barchart.render()
+oszlopdiagram = pygal.Bar ()
+oszlopdiagram.title = 'Kedvenc kisállat'
+oszlopdiagram.add('Kutya', 6)
+oszlopdiagram.add ('Macska', 4)
+oszlopdiagram.add('Hörcsög', 3)
+oszlopdiagram.add('Hal', 2)
+oszlopdiagram.add ('Kígyó', 1)
+oszlopdiagram.render ()
   
-piechart2 = pygal.Pie()
-barchart2 = pygal.Bar()
+kordiagram2 = pygal.Pie()
+oszlopdiagram2 = pygal.Bar()
 
 file = open('pets.txt', 'r')
 
-for line in file.read().splitlines():
-  if line:
-    label, value = line.split(' ')
-    piechart2.add(label, int(value))
-    barchart2.add(label, int(value))
+for sor in file.read().splitlines():
+  if sor:
+    cimke, ertek = sor.split(' ')
+    kordiagram2.add(cimke, int(ertek))
+    oszlopdiagram2.add(cimke, int(ertek))
     
 file.close()
 
-#piechart2.render()
-#barchart2.render()
+#kordiagram2.render()
+#oszlopdiagram2.render()
 
-butterflies = pygal.Bar()
-butterflies.title = 'Butterfly Count'
+pillangok = pygal.Bar()
+pillangok.title = 'Pillangók száma'
 
 file = open('butterflies.txt', 'r')
 
-for line in file.read().splitlines():
-  if line:
-    label, value = line.split(': ')
-    butterflies.add(label, int(value))
+for sor in file.read().splitlines():
+  if sor:
+    cimke, ertek = sor.split ('')
+    pillangok.add(cimke, int(ertek))
 file.close()
 
-butterflies.render()
+pillangok.render()
 
-pn = pygal.Bar()
-pn.title = 'Pirates vs Ninjas'
+kalozok = pygal.Bar()
+kalozok.title = 'Kalózok vagy nindzsák'
 
 file = open('piratesninjas.txt', 'r')
 
-for line in file.read().splitlines():
-  if line:
-    label, value = line.split(' ')
-    pn.add(label, int(value))
+for sor in file.read().splitlines():
+  if sor:
+    cimke, ertek = sor.split(' ')
+    kalozok.add(cimke, int(ertek))
 file.close()
 
-pn.render()
+kalozok.render()
